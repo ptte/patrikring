db8d8a73b2ac923f14b2c61c4dba5ac01664e9c1
Patrik Ring <me@patrikring.se>
2013-07-25 18:08:26 -0700

    initial commit of app

---
+++ index.js
@@ -0,0 +1,37 @@
+var Router = require('routes');
+var http = require('http');
+var connect = require('connect');
+
+var AppBootstrap = function () {};
+
+// Routing //
+
+var routers = {};
+routers['post'] = Router();
+routers['get']  = Router();
+
+routers['get'].addRoute("/", "index");
+
+// Controllers //
+
+// Post-receives from github
+// receives a github payload
+
+// core-app
+AppBootstrap.prototype.postReceiveCoreApp = function (req, res, route) {
+  res.end('Ok');
+}
+
+// The app //
+
+var App = new AppBootstrap();
+
+var connection_handler = connect()
+  .use(connect.bodyParser())
+  .use(function(req, res){
+    var method = req.method.toLowerCase();
+    var route = routers[method].match(req.url);
+    App[route.fn](req, res, route);
+  });
+
+http.createServer(connection_handler).listen(1337);
